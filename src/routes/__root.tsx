f022c3df61762f753904a7d97f9d2f0e6ecb9502
YILDIRIM <yldrm4238@gmail.com>
2026-06-26 19:50:14 +0200

    chore: add Meta Pixel tracking code

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,6 +102,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
     ],
+    scripts: [
+      {
+        innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1219643051228047');fbq('track','PageView');`,
+      },
+    ],
   }),
   shellComponent: RootShell,
   component: RootComponent,
